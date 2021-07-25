b92b43726b1caa98b4a00bfeb4a9f77d9e7f290a
JXPropertyCode <yellowdice92@hotmail.com>
2021-07-24 18:45:32 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [timerOn, setTimerOn] = useState(false)
+  const countRef = useRef(null)
+
+  // this will run when the components render
+  useEffect(() => {
+    // let interval = null; // gonna be the same hook when we turn it off and on
+
+    if (timerOn) {
+      countRef.current = setInterval(() => {
+        // console.log("Running")
+        setTime(prevTime => prevTime + 10) // prevTime is arbituary
+      }, 10)
+    } else{
+      clearInterval(countRef.current)
+    }
+
+    // clean up function, when the component gets unmounted/user leaves page
+    // prevents memory leaks
+    return () => clearInterval(countRef.current)
+
+  }, [timerOn]) // if timerOn changes, it will run
+
+  console.log(time)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+
+        {/* Minutes */}
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+
+        {/* seconds */}
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+
+
+        {/* modulo since when it goes to 100, we want it to go back to 0 
+          Slice removes if there is more than 2 digits. So the 0 will go away
+        */}
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+
+        {!timerOn && time === 0 && (
+          <button onClick={ () => setTimerOn(true) }>Start</button>
+        )}
+
+        {timerOn && (
+          <button onClick={ () => setTimerOn(false) }>Stop</button>
+        )}
+
+        {!timerOn && time !== 0 && (
+          <button onClick={ () => setTimerOn(true) }>Resume</button>
+        )}
+        
+        {!timerOn && time > 0 && (
+          <button onClick={ () => setTime(0) }>Reset</button>
+        )}
+        
+      </div>
+      
     </div>
   );
 }
